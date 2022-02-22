7b9d27e13171e52dc58f87a51c620e1b245ff873
Adrien <adrien.brochier@imj-prg.fr>
2022-02-22 17:52:44 +0100

    Tue 22 Feb 2022 05:52:44 PM CET

--- imaptw.py
+++ imaptw.py
@@ -3,8 +3,8 @@ from taskw import TaskWarrior
 import email
 from email.header import Header, decode_header, make_header
 
-server=IMAPClient("imap.mysrver.yo")
-server.login('thisismyname', 'thisismypassword')
+server=IMAPClient("imapserver.yo")
+server.login('login', 'pass')
 server.select_folder("INBOX")
 messages = server.search("FLAGGED")
 w=TaskWarrior()
@@ -14,7 +14,8 @@ for uid, message_data in server.fetch(messages, "RFC822").items():
     n=next((item for item in tsk if 'mailid' in item and item['mailid'] == str(uid)), False)
     if n==False:
         email_message = email.message_from_bytes(message_data[b"RFC822"])
-        w.task_add(repr(str(make_header(decode_header(email_message.get("From")))))+" "+repr(str(make_header(decode_header(email_message.get("Subject"))))),tag="email", mailid=uid)
+        if email_message.get("Subject") is not None:
+            w.task_add(repr(str(make_header(decode_header(email_message.get("From")))))+" "+repr(str(make_header(decode_header(email_message.get("Subject"))))),tag="email", mailid=uid)
     else:
         if n['status']=='completed':
            clean.append(uid)
